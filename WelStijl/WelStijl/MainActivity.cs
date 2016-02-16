d9a8b2ee415bbc5482ff4f29a58020daffdbeb9d
Sven Dubbeld <s.dubbeld@student.fontys.nl>
2016-02-16 15:34:07 +0100

    Initial Visual Studio Project

---
+++ WelStijl/WelStijl/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace WelStijl
+{
+    [Activity(Label = "WelStijl", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/AppTheme")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
