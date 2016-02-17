40744d7ca7dcdb92a66bd6f76f89a77643fbba65
Sven Dubbeld <s.dubbeld@student.fontys.nl>
2016-02-17 14:17:49 +0100

    Change colors

--- WelStijl/WelStijl/Resources/Resource.Designer.cs
+++ WelStijl/WelStijl/Resources/Resource.Designer.cs
@@ -913,32 +913,32 @@ namespace WelStijl
 		public partial class Color
 		{
 			
+			// aapt resource value: 0x7f0a004b
+			public const int abc_background_cache_hint_selector_material_dark = 2131361867;
+			
 			// aapt resource value: 0x7f0a004c
-			public const int abc_background_cache_hint_selector_material_dark = 2131361868;
+			public const int abc_background_cache_hint_selector_material_light = 2131361868;
 			
 			// aapt resource value: 0x7f0a004d
-			public const int abc_background_cache_hint_selector_material_light = 2131361869;
-			
-			// aapt resource value: 0x7f0a004e
-			public const int abc_color_highlight_material = 2131361870;
+			public const int abc_color_highlight_material = 2131361869;
 			
 			// aapt resource value: 0x7f0a0000
 			public const int abc_input_method_navigation_guard = 2131361792;
 			
+			// aapt resource value: 0x7f0a004e
+			public const int abc_primary_text_disable_only_material_dark = 2131361870;
+			
 			// aapt resource value: 0x7f0a004f
-			public const int abc_primary_text_disable_only_material_dark = 2131361871;
+			public const int abc_primary_text_disable_only_material_light = 2131361871;
 			
 			// aapt resource value: 0x7f0a0050
-			public const int abc_primary_text_disable_only_material_light = 2131361872;
+			public const int abc_primary_text_material_dark = 2131361872;
 			
 			// aapt resource value: 0x7f0a0051
-			public const int abc_primary_text_material_dark = 2131361873;
+			public const int abc_primary_text_material_light = 2131361873;
 			
 			// aapt resource value: 0x7f0a0052
-			public const int abc_primary_text_material_light = 2131361874;
-			
-			// aapt resource value: 0x7f0a0053
-			public const int abc_search_url_text = 2131361875;
+			public const int abc_search_url_text = 2131361874;
 			
 			// aapt resource value: 0x7f0a0001
 			public const int abc_search_url_text_normal = 2131361793;
@@ -949,11 +949,11 @@ namespace WelStijl
 			// aapt resource value: 0x7f0a0003
 			public const int abc_search_url_text_selected = 2131361795;
 			
-			// aapt resource value: 0x7f0a0054
-			public const int abc_secondary_text_material_dark = 2131361876;
+			// aapt resource value: 0x7f0a0053
+			public const int abc_secondary_text_material_dark = 2131361875;
 			
-			// aapt resource value: 0x7f0a0055
-			public const int abc_secondary_text_material_light = 2131361877;
+			// aapt resource value: 0x7f0a0054
+			public const int abc_secondary_text_material_light = 2131361876;
 			
 			// aapt resource value: 0x7f0a0004
 			public const int accent_material_dark = 2131361796;
@@ -997,17 +997,17 @@ namespace WelStijl
 			// aapt resource value: 0x7f0a0011
 			public const int button_material_light = 2131361809;
 			
-			// aapt resource value: 0x7f0a0046
-			public const int colorAccent = 2131361862;
-			
-			// aapt resource value: 0x7f0a004b
-			public const int colorActionBarTitle = 2131361867;
+			// aapt resource value: 0x7f0a0045
+			public const int colorAccent = 2131361861;
 			
 			// aapt resource value: 0x7f0a004a
-			public const int colorDivider = 2131361866;
+			public const int colorActionBarTitle = 2131361866;
 			
 			// aapt resource value: 0x7f0a0049
-			public const int colorIcons = 2131361865;
+			public const int colorDivider = 2131361865;
+			
+			// aapt resource value: 0x7f0a0048
+			public const int colorIcons = 2131361864;
 			
 			// aapt resource value: 0x7f0a0043
 			public const int colorPrimary = 2131361859;
@@ -1015,14 +1015,11 @@ namespace WelStijl
 			// aapt resource value: 0x7f0a0044
 			public const int colorPrimaryDark = 2131361860;
 			
-			// aapt resource value: 0x7f0a0045
-			public const int colorPrimaryLight = 2131361861;
+			// aapt resource value: 0x7f0a0046
+			public const int colorPrimaryText = 2131361862;
 			
 			// aapt resource value: 0x7f0a0047
-			public const int colorPrimaryText = 2131361863;
-			
-			// aapt resource value: 0x7f0a0048
-			public const int colorSecondaryText = 2131361864;
+			public const int colorSecondaryText = 2131361863;
 			
 			// aapt resource value: 0x7f0a003a
 			public const int design_fab_shadow_end_color = 2131361850;
@@ -1165,11 +1162,11 @@ namespace WelStijl
 			// aapt resource value: 0x7f0a0037
 			public const int switch_thumb_disabled_material_light = 2131361847;
 			
-			// aapt resource value: 0x7f0a0056
-			public const int switch_thumb_material_dark = 2131361878;
+			// aapt resource value: 0x7f0a0055
+			public const int switch_thumb_material_dark = 2131361877;
 			
-			// aapt resource value: 0x7f0a0057
-			public const int switch_thumb_material_light = 2131361879;
+			// aapt resource value: 0x7f0a0056
+			public const int switch_thumb_material_light = 2131361878;
 			
 			// aapt resource value: 0x7f0a0038
 			public const int switch_thumb_normal_material_dark = 2131361848;
